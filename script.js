ba833ce557bc5af2b668295899e6e57c5a7e7196
Wellson Gideon Tandy <wellsongideont@gmail.com>
2023-02-06 15:18:26 +0700

    first commit - committing all files to the repository

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: false,
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+});
+
+
